c1edeaafc1d3bcb8c9f4d278ed50b0da27bc2eb7
irisfaraway <iris.faraway@gmail.com>
2016-08-10 11:54:44 +0100

    exercise 10

---
+++ ex10.rb
@@ -0,0 +1,28 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+# Study drill 2
+puts '''
+She said, "Hello."
+"Hey," I replied.
+'''
+
+# Study drill 3
+puts "Here's my house:"
+puts """
+\u25E2\u25E3
+\u2593\u2593
+"""
